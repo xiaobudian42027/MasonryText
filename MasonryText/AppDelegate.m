e58dfb6e32fc640842223bec030c8800d224ae37
xiaobudian42027 <zhangguoqingxie@163.com>
2017-01-23 14:50:39 +0800

    Marst和分支互相合并01测试

--- MasonryText/AppDelegate.m
+++ MasonryText/AppDelegate.m
@@ -46,6 +46,7 @@
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    //测试分支和marst互相合并01
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
