476c6cb412c1b003ecce8226abfa951a8d47a5ab
xiaobudian42027 <zhangguoqingxie@163.com>
2017-02-08 10:37:33 +0800

    002

--- MasonryText/Header.h
+++ MasonryText/Header.h
@@ -11,14 +11,14 @@
 
 
 #ifndef YYSYNTH_DYNAMIC_PROPERTY_OBJECT
-#define YYSYNTH_DYNAMIC_PROPERTY_OBJECT(_getter_,_setter_,_association_,_type_)\
--(void)_setter_:(_type_)object{\
-  [self willChangeValueForKey:@#_getter_];\
-  objc_setAssociatedObject(self,_cmd,object,OBJC_ASSOCIATION_##_association_);\
-  [self didChangeValueForKey:@#_getter_];\
-}\
--(_type_)_getter_{\
-  return objc_getAssociatedObject(self,@selector(_setter_:))\
+#define YYSYNTH_DYNAMIC_PROPERTY_OBJECT(_getter_, _setter_, _association_, _type_) \
+- (void)_setter_ : (_type_)object { \
+[self willChangeValueForKey:@#_getter_]; \
+objc_setAssociatedObject(self, _cmd, object, OBJC_ASSOCIATION_ ## _association_); \
+[self didChangeValueForKey:@#_getter_]; \
+} \
+- (_type_)_getter_ { \
+return objc_getAssociatedObject(self, @selector(_setter_:)); \
 }
 #endif
 
